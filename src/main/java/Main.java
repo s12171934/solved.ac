4c2863bb5c972d0c891bd239638c821dd26e5f49
s12171934 <ssong99999@naver.com>
2024-04-21 20:22:36 +0900

    4153 직각삼각형

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,22 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = bufferedReader.readLine();
-        int n = Integer.parseInt(bufferedReader.readLine());
-        bufferedReader.close();
 
-        System.out.println(str.charAt(n - 1));
+        while (true) {
+            String str = bufferedReader.readLine();
+            if(str == null || str.equals("0 0 0")) break;
+            System.out.println(rightTriangle(str) ? "right" : "wrong");
+        }
+
+        bufferedReader.close();
+    }
+
+    static boolean rightTriangle(String str) {
+        String[] nums = str.split(" ");
+        double a = Math.pow(Integer.parseInt(nums[0]),2);
+        double b = Math.pow(Integer.parseInt(nums[1]),2);
+        double c = Math.pow(Integer.parseInt(nums[2]),2);
+
+        return a + b == c || b + c == a || c + a == b;
     }
 }
