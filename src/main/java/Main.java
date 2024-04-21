6c6c0402a9d5ec6d14197f59ba3a9ca16e975cdd
s12171934 <ssong99999@naver.com>
2024-04-21 19:58:20 +0900

    11720 숫자의 합

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,17 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String str = bufferedReader.readLine();
-        bufferedReader.close();
+        int n = Integer.parseInt(bufferedReader.readLine());
 
-        System.out.println((int)str.charAt(0));
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = bufferedReader.read() - 48;
+            result += num;
+        }
+
+        System.out.println(result);
+
+        bufferedReader.close();
     }
 }
