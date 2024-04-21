8ccf61ae098bf270418d823e640fe3eaec2549d7
s12171934 <ssong99999@naver.com>
2024-04-21 21:41:25 +0900

    1259 팰린드롬수

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        int m = Integer.parseInt(stringTokenizer.nextToken());
-        int result = 0;
 
-        String[] nums = bufferedReader.readLine().split(" ");
+        while (true) {
+            String str = bufferedReader.readLine();
+            if(str.equals("0")) break;
 
-        for (int i = 0; i < n - 2; i++) {
-            int numI = Integer.parseInt(nums[i]);
-            for (int j = i + 1; j < n - 1; j++) {
-                int numJ = Integer.parseInt(nums[j]);
-                if (numI + numJ >= m) {
-                    continue;
-                }
-                for (int k = j + 1; k < n; k++) {
-                    int numK = Integer.parseInt(nums[k]);
-                    if (numI + numJ + numK <= m) {
-                        result = Math.max(result, numI + numJ + numK);
-                    }
+            String result = "yes";
+
+            for (int i = 0; i < str.length() / 2; i++) {
+                if (str.charAt(i) != str.charAt(str.length() - i - 1)){
+                    result = "no";
+                    break;
                 }
             }
-        }
 
-        System.out.println(result);
+            System.out.println(result);
+
+        }
 
         bufferedReader.close();
     }
