825f7e91c98100823380e4c35fb62e42fbf3cca3
s12171934 <ssong99999@naver.com>
2024-04-21 19:42:23 +0900

    10950 A+B

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,17 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = bufferedReader.readLine().split(" ");
+        int n = Integer.parseInt(bufferedReader.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] str = bufferedReader.readLine().split(" ");
+
+            int a = Integer.parseInt(str[0]);
+            int b = Integer.parseInt(str[1]);
+
+            System.out.println(a + b);
+        }
+
         bufferedReader.close();
-
-        int a = Integer.parseInt(str[0]);
-        int b = Integer.parseInt(str[1]);
-
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a * b);
-        System.out.println(a / b);
-        System.out.println(a % b);
     }
 }
