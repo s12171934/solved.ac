c8880999d6e2e5ec1165bfd891ef2faa19a4f483
s12171934 <ssong99999@naver.com>
2024-04-21 21:23:40 +0900

    2798 블랙잭

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        int result = 0;
+
         String[] nums = bufferedReader.readLine().split(" ");
 
-        for (String num : nums) {
-            int n = Integer.parseInt(num);
-
-            if (n == 1) {
-                count--;
-                continue;
-            }
-
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if(n % i == 0){
-                    count--;
-                    break;
+        for (int i = 0; i < n - 2; i++) {
+            int numI = Integer.parseInt(nums[i]);
+            for (int j = i + 1; j < n - 1; j++) {
+                int numJ = Integer.parseInt(nums[j]);
+                if (numI + numJ >= m) {
+                    continue;
+                }
+                for (int k = j + 1; k < n; k++) {
+                    int numK = Integer.parseInt(nums[k]);
+                    if (numI + numJ + numK <= m) {
+                        result = Math.max(result, numI + numJ + numK);
+                    }
                 }
             }
         }
 
-        System.out.println(count);
+        System.out.println(result);
+
         bufferedReader.close();
     }
 }
