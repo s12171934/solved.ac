040abc009604f995d05c921bcca78139ab083014
s12171934 <ssong99999@naver.com>
2024-04-21 20:41:50 +0900

    1978 소수 찾기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,22 +5,26 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(bufferedReader.readLine());
+        String[] nums = bufferedReader.readLine().split(" ");
 
-        while (true) {
-            String str = bufferedReader.readLine();
-            if(str == null || str.equals("0 0 0")) break;
-            System.out.println(rightTriangle(str) ? "right" : "wrong");
+        for (String num : nums) {
+            int n = Integer.parseInt(num);
+
+            if (n == 1) {
+                count--;
+                continue;
+            }
+
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if(n % i == 0){
+                    count--;
+                    break;
+                }
+            }
         }
 
+        System.out.println(count);
         bufferedReader.close();
-    }
-
-    static boolean rightTriangle(String str) {
-        String[] nums = str.split(" ");
-        double a = Math.pow(Integer.parseInt(nums[0]),2);
-        double b = Math.pow(Integer.parseInt(nums[1]),2);
-        double c = Math.pow(Integer.parseInt(nums[2]),2);
-
-        return a + b == c || b + c == a || c + a == b;
     }
 }
