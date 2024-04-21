407377af34dedebc2ab456327630745074109796
s12171934 <ssong99999@naver.com>
2024-04-21 19:25:57 +0900

    2739 구구단

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int t = Integer.parseInt(bufferedReader.readLine());
-        String[] str;
-
-        for (int i = 0; i < t; i++) {
-            str = bufferedReader.readLine().split(" ");
-            int p = Integer.parseInt(str[0]);
-            String s = str[1];
-
-            String result = "";
-            for (int j = 0; j < s.length(); j++) {
-                result += String.valueOf(s.charAt(j)).repeat(p);
-            }
-            System.out.println(result);
-        }
-
+        int n = Integer.parseInt(bufferedReader.readLine());
         bufferedReader.close();
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
     }
 }
