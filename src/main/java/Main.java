fd21589f33ed510e0c29d6138954251764837eb9
s12171934 <ssong99999@naver.com>
2024-04-22 00:58:29 +0900

    10814 나이순 정렬

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder stringBuilder = new StringBuilder();
-
         int n = Integer.parseInt(bufferedReader.readLine());
 
-        ArrayList<Integer> nums = new ArrayList<>();
+        String[] str = new String[n];
         for (int i = 0; i < n; i++) {
-            nums.add(Integer.parseInt(bufferedReader.readLine()));
+            str[i] = bufferedReader.readLine();
         }
-        Collections.sort(nums);
 
         bufferedReader.close();
 
-        for(int value : nums) {
-            stringBuilder.append(value).append('\n');
+        for(Object s : Arrays.stream(str).sorted(Main::compare).toArray()){
+            stringBuilder.append(s).append("\n");
         }
+
         System.out.println(stringBuilder);
+    }
+
+    static int compare(String s1, String s2) {
+        int age1 = Integer.parseInt(s1.split(" ")[0]);
+        int age2 = Integer.parseInt(s2.split(" ")[0]);
+
+        return age1 - age2;
     }
 }
