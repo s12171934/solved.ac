5c6d9b39873e1ccd218e3a5bb8bfbec183ed5001
s12171934 <ssong99999@naver.com>
2024-04-22 14:43:56 +0900

    9012 괄호

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder stringBuilder = new StringBuilder();
+
         int n = Integer.parseInt(bufferedReader.readLine());
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        Map<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
-            String target = stringTokenizer.nextToken();
-            map.put(target, map.getOrDefault(target, 0) + 1);
+            boolean checkVPS = checkVPS(bufferedReader.readLine());
+            stringBuilder.append(checkVPS ? "YES" : "NO" ).append("\n");
         }
 
-        int m = Integer.parseInt(bufferedReader.readLine());
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
-        for (int i = 0; i < m; i++) {
-            stringBuilder.append(map.getOrDefault(stringTokenizer.nextToken(),0)).append(" ");
-        }
         System.out.println(stringBuilder);
+        bufferedReader.close();
+    }
+
+    static boolean checkVPS(String str) {
+        if (str.length() % 2 != 0) {
+            return false;
+        }
+
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            if(c == '('){
+                count++;
+            } else {
+                count--;
+            }
+
+            if(count < 0) {
+                return false;
+            }
+        }
+
+        return count == 0;
     }
 }
