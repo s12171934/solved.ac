7602cd99516259395b41ac01bb403b52f32229f3
s12171934 <ssong99999@naver.com>
2024-04-23 01:09:47 +0900

    1764 듣보잡

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
-
-        Arrays.sort(nums);
-        int result = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] str = bufferedReader.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int m = Integer.parseInt(str[1]);
+        Set<String> noListen = new HashSet<>();
+        Set<String> noSee = new HashSet<>();
 
         for (int i = 0; i < n; i++) {
-            result += (n - i) * nums[i];
+            noListen.add(bufferedReader.readLine());
         }
 
-        System.out.println(result);
+        for (int i = 0; i < m; i++) {
+            noSee.add(bufferedReader.readLine());
+        }
+
+        noSee.retainAll(noListen);
+        stringBuilder.append(noSee.size()).append("\n");
+        for (Object s : noSee.stream().sorted().toArray()) {
+            stringBuilder.append(s).append("\n");
+        }
+
+        System.out.println(stringBuilder);
         bufferedReader.close();
     }
 }
