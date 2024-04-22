ea7a968273aedbd9ae1461ca235e652f1df95b87
s12171934 <ssong99999@naver.com>
2024-04-22 16:39:20 +0900

    10845 큐

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder stringBuilder = new StringBuilder();
         int n = Integer.parseInt(bufferedReader.readLine());
-        Stack<String> stack = new Stack<>();
+        int[] queue = new int[n];
+        int size = 0;
 
         for (int i = 0; i < n; i++) {
             String[] str = bufferedReader.readLine().split(" ");
 
             switch (str[0]) {
                 case "push" :
-                    stack.push(str[1]);
+                    queue[size] = Integer.parseInt(str[1]);
+                    size++;
                     break;
 
                 case "pop" :
-                    stringBuilder.append(stack.isEmpty() ? -1 : stack.pop()).append("\n");
+                    stringBuilder.append(queue[0] == 0 ? -1 : queue[0]).append("\n");
+                    if (queue[0] == 0) break;
+
+                    for (int j = 0; j < size - 1; j++) {
+                        queue[j] = queue[j + 1];
+                    }
+                    queue[size - 1] = 0;
+                    size--;
                     break;
 
                 case "size" :
-                    stringBuilder.append(stack.size()).append("\n");
+                    stringBuilder.append(size).append("\n");
                     break;
 
                 case "empty" :
-                    stringBuilder.append(stack.isEmpty() ? 1 : 0).append("\n");
+                    stringBuilder.append(queue[0] == 0 ? 1 : 0).append("\n");
                     break;
 
-                case "top" :
-                    stringBuilder.append(stack.isEmpty() ? -1 : stack.peek()).append("\n");
+                case "front" :
+                    stringBuilder.append(queue[0] == 0 ? -1 : queue[0]).append("\n");
+                    break;
+
+                case "back" :
+                    stringBuilder.append(queue[0] == 0 ? -1 : queue[size - 1]).append("\n");
                     break;
             }
 
