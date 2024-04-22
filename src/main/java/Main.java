a58367e3bd59e39ae0c51e94e6997e9d8fb57866
s12171934 <ssong99999@naver.com>
2024-04-22 16:48:06 +0900

    10866 덱

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,26 +8,42 @@ public class Main {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder stringBuilder = new StringBuilder();
         int n = Integer.parseInt(bufferedReader.readLine());
-        int[] queue = new int[n];
+        int[] deque = new int[n];
         int size = 0;
 
         for (int i = 0; i < n; i++) {
             String[] str = bufferedReader.readLine().split(" ");
 
             switch (str[0]) {
-                case "push" :
-                    queue[size] = Integer.parseInt(str[1]);
+                case "push_front" :
+                    for (int j = size - 1; j >= 0; j--) {
+                        deque[j + 1] = deque[j];
+                    }
+                    deque[0] = Integer.parseInt(str[1]);
                     size++;
                     break;
 
-                case "pop" :
-                    stringBuilder.append(queue[0] == 0 ? -1 : queue[0]).append("\n");
-                    if (queue[0] == 0) break;
+                case "push_back" :
+                    deque[size] = Integer.parseInt(str[1]);
+                    size++;
+                    break;
+
+                case "pop_front" :
+                    stringBuilder.append(deque[0] == 0 ? -1 : deque[0]).append("\n");
+                    if (deque[0] == 0) break;
 
                     for (int j = 0; j < size - 1; j++) {
-                        queue[j] = queue[j + 1];
+                        deque[j] = deque[j + 1];
                     }
-                    queue[size - 1] = 0;
+                    deque[size - 1] = 0;
+                    size--;
+                    break;
+
+                case "pop_back" :
+                    stringBuilder.append(deque[0] == 0 ? -1 : deque[size - 1]).append("\n");
+                    if (deque[0] == 0) break;
+
+                    deque[size - 1] = 0;
                     size--;
                     break;
 
@@ -36,15 +52,15 @@ public class Main {
                     break;
 
                 case "empty" :
-                    stringBuilder.append(queue[0] == 0 ? 1 : 0).append("\n");
+                    stringBuilder.append(deque[0] == 0 ? 1 : 0).append("\n");
                     break;
 
                 case "front" :
-                    stringBuilder.append(queue[0] == 0 ? -1 : queue[0]).append("\n");
+                    stringBuilder.append(deque[0] == 0 ? -1 : deque[0]).append("\n");
                     break;
 
                 case "back" :
-                    stringBuilder.append(queue[0] == 0 ? -1 : queue[size - 1]).append("\n");
+                    stringBuilder.append(deque[0] == 0 ? -1 : deque[size - 1]).append("\n");
                     break;
             }
 
