a8ea69aa90c7501b688e7cb0eb55069be6c508ea
s12171934 <ssong99999@naver.com>
2024-04-23 20:56:14 +0900

    18870 좌표 압축

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder stringBuilder = new StringBuilder();
-        String[] str = bufferedReader.readLine().split(" ");
-        int n = Integer.parseInt(str[0]);
-        int m = Integer.parseInt(str[1]);
-        Set<String> noListen = new HashSet<>();
-        Set<String> noSee = new HashSet<>();
+
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[] answer = new int[n];
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(stringTokenizer.nextToken());
+            answer[i] = num;
+        }
+
+        int[] sort = Arrays.stream(answer).distinct().sorted().toArray();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < sort.length; i++) {
+            map.put(sort[i],i);
+        }
+
 
         for (int i = 0; i < n; i++) {
-            noListen.add(bufferedReader.readLine());
-        }
-
-        for (int i = 0; i < m; i++) {
-            noSee.add(bufferedReader.readLine());
-        }
-
-        noSee.retainAll(noListen);
-        stringBuilder.append(noSee.size()).append("\n");
-        for (Object s : noSee.stream().sorted().toArray()) {
-            stringBuilder.append(s).append("\n");
+            stringBuilder.append(map.get(answer[i])).append(" ");
         }
 
         System.out.println(stringBuilder);
