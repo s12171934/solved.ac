4e08c0f998873318537951888e56a32f24ccf459
s12171934 <ssong99999@naver.com>
2024-04-26 17:54:40 +0900

    11726 2xn 타일링

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,53 +4,30 @@ import java.io.InputStreamReader;
 
 
 public class Main {
-    static int[][] board;
-    static int[][] dist = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder();
-        int t = Integer.parseInt(bufferedReader.readLine());
-        for (int i = 0; i < t; i++) {
-            String[] str = bufferedReader.readLine().split(" ");
-            int m = Integer.parseInt(str[0]);
-            int n = Integer.parseInt(str[1]);
-            int k = Integer.parseInt(str[2]);
-            board = new int[m][n];
-
-            for (int j = 0; j < k; j++) {
-                String[] pixel = bufferedReader.readLine().split(" ");
-                board[Integer.parseInt(pixel[0])][Integer.parseInt(pixel[1])] = 1;
-            }
-
-            stringBuilder.append(countBugs(board)).append("\n");
-        }
-
-        System.out.println(stringBuilder);
+        int n = Integer.parseInt(bufferedReader.readLine());
+        arr = new int[n];
+        System.out.println(tile(n));
         bufferedReader.close();
     }
 
-    static int countBugs(int[][] board) {
-        int count = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == 1) {
-                    count++;
-                    board[i][j] = 0;
-                    DFS(i,j);
-                }
+    static int tile(int n) {
+        if(arr[n -1] == 0){
+            if (n == 1) {
+               arr[0] = 1;
+               return 1;
             }
-        }
-        return count;
-    }
 
-    static void DFS(int i, int j) {
-        for (int[] d : dist) {
-            int dx = i + d[0];
-            int dy = j + d[1];
-            if(dx >= 0 && dx < board.length && dy >= 0 && dy < board[0].length && board[dx][dy] == 1) {
-                board[dx][dy] = 0;
-                DFS(dx,dy);
+            if (n == 2) {
+                arr[1] = 2;
+                return 2;
             }
+            arr[n - 1] = (tile(n - 2) + tile(n - 1)) % 10007;
+            return  arr[n -1];
         }
+
+        return arr[n -1];
     }
 }
