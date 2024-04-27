932dc7f4ea801819f1b5595702d271d1091adf3e
s12171934 <ssong99999@naver.com>
2024-04-27 21:16:03 +0900

    11724 연결 요소의 개수

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 public class Main {
-    static int[] arr;
+    static int[][] grid;
+    static boolean[] node;
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        arr = new int[n];
-        System.out.println(tile(n));
+        StringTokenizer stringTokenizer;
+        String[] str = bufferedReader.readLine().split(" ");
+        n = Integer.parseInt(str[0]);
+        int m = Integer.parseInt(str[1]);
+        int count = 0;
+
+        grid = new int[n + 1][n + 1];
+        node = new boolean[n + 1];
+
+        for(int i = 0; i < m; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+
+            grid[x][y] = 1;
+            grid[y][x] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!node[i]) {
+                DFS(i);
+               count++;
+            }
+        }
+
+        System.out.println(count);
         bufferedReader.close();
     }
 
-    static int tile(int n) {
-        if(arr[n -1] == 0){
-            if (n == 1) {
-               arr[0] = 1;
-               return 1;
+    static void DFS(int value) {
+        node[value] = true;
+        for (int i = 1; i <= n; i++) {
+            if (grid[value][i] == 1 && !node[i]) {
+                DFS(i);
             }
-
-            if (n == 2) {
-                arr[1] = 2;
-                return 2;
-            }
-            arr[n - 1] = (tile(n - 2) + tile(n - 1)) % 10007;
-            return  arr[n -1];
         }
-
-        return arr[n -1];
     }
 }
