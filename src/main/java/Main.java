5a368549a8a1ea45a407bbdea937723e33a70de8
s12171934 <ssong99999@naver.com>
2024-04-28 19:17:24 +0900

    1463 1로 만들기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,65 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
 
 public class Main {
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder();
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        int m = Integer.parseInt(stringTokenizer.nextToken());
-        int startX = 0;
-        int startY = 0;
-        int[][] dists = new int[][]{{0 , 1}, {0 , -1}, {1, 0}, {-1, 0}};
-        int[][] map = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            for (int j = 0; j < m; j++) {
-                int elem = Integer.parseInt(stringTokenizer.nextToken());
-                if (elem == 2) {
-                    startY = i;
-                    startX = j;
-                }
-                map[i][j] = elem;
-            }
+        int N = Integer.parseInt(bufferedReader.readLine());
+        dp = new int[N + 1];
+        for (int i = 0; i < N; i++) {
+            makeOne(i + 1);
         }
-
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{startY, startX});
-        map[startY][startX] = 0;
-
-        while (!queue.isEmpty()) {
-            int[] node = queue.poll();
-            for (int[] dist : dists) {
-                int y = node[0] + dist[0];
-                int x = node[1] + dist[1];
-                if (y < 0 || y >= n || x < 0 || x >= m) continue;
-                if (visited[y][x] || map[y][x] == 0) continue;
-                queue.add(new int[]{y, x});
-                visited[y][x] = true;
-                map[y][x] = map[node[0]][node[1]] + 1;
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visited[i][j] && map[i][j] == 1) {
-                    stringBuilder.append(-1).append(" ");
-                }
-                else {
-                    stringBuilder.append(map[i][j]).append(" ");
-                }
-            }
-            stringBuilder.append("\n");
-        }
-        System.out.println(stringBuilder);
+        System.out.println(makeOne(N));
         bufferedReader.close();
+    }
+
+    static int makeOne(int N) {
+        if (N != 1 && dp[N] == 0) {
+            if (N % 6 == 0) {
+                dp[N] = Math.min(Math.min(makeOne(N / 3), makeOne(N / 2)), makeOne(N - 1)) + 1;
+            } else if (N % 3 == 0) {
+                dp[N] = Math.min(makeOne(N / 3), makeOne(N - 1)) + 1;
+            } else if (N % 2 == 0) {
+                dp[N] = Math.min(makeOne(N / 2), makeOne(N - 1)) + 1;
+            } else {
+                dp[N] = makeOne(N - 1) + 1;
+            }
+        }
+        return dp[N];
     }
 }
