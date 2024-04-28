a5d3b5a3c54aeeed15ad0f22b990cddd0aff722c
s12171934 <ssong99999@naver.com>
2024-04-28 20:31:13 +0900

    7576 토마토

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        dp = new int[N + 1];
-        for (int i = 0; i < N; i++) {
-            makeOne(i + 1);
-        }
-        System.out.println(makeOne(N));
-        bufferedReader.close();
-    }
+        StringTokenizer stringTokenizer;
+        String[] str = bufferedReader.readLine().split(" ");
+        int N = Integer.parseInt(str[0]);
+        int M = Integer.parseInt(str[1]);
+        int[][] dists = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int[][] tomato = new int[M][N];
+        boolean[][] visited = new boolean[M][N];
+        Queue<int[]> queue = new LinkedList<>();
 
-    static int makeOne(int N) {
-        if (N != 1 && dp[N] == 0) {
-            if (N % 6 == 0) {
-                dp[N] = Math.min(Math.min(makeOne(N / 3), makeOne(N / 2)), makeOne(N - 1)) + 1;
-            } else if (N % 3 == 0) {
-                dp[N] = Math.min(makeOne(N / 3), makeOne(N - 1)) + 1;
-            } else if (N % 2 == 0) {
-                dp[N] = Math.min(makeOne(N / 2), makeOne(N - 1)) + 1;
-            } else {
-                dp[N] = makeOne(N - 1) + 1;
+        for (int i = 0; i < M; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < N; j++) {
+                int elem = Integer.parseInt(stringTokenizer.nextToken());
+                tomato[i][j] = elem;
+                if (elem == 1) {
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = true;
+                    tomato[i][j] = 0;
+                }
             }
         }
-        return dp[N];
+
+        while (!queue.isEmpty()) {
+            int[] node = queue.poll();
+            for (int[] dist : dists) {
+                int y = node[0] + dist[0];
+                int x = node[1] + dist[1];
+
+                if (y < 0 || x < 0 || y >= M || x >= N) continue;
+                if (tomato[y][x] == -1) continue;
+                if (visited[y][x]) continue;
+
+                queue.add(new int[]{y, x});
+                tomato[y][x] = tomato[node[0]][node[1]] + 1;
+                visited[y][x] = true;
+            }
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (tomato[i][j] != -1 && !visited[i][j]) {
+                    System.out.println(-1);
+                    return;
+                }
+                result = Math.max(result, tomato[i][j]);
+            }
+        }
+
+        System.out.println(result);
+        bufferedReader.close();
     }
 }
