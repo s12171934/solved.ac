9578bfbe28eb8d7326a87c22425fbd5189cf4485
s12171934 <ssong99999@naver.com>
2024-04-28 21:23:25 +0900

    15654 N과 M

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static StringBuilder stringBuilder = new StringBuilder();
+    static int N;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String[] str = bufferedReader.readLine().split(" ");
-        int N = Integer.parseInt(str[0]);
+        N = Integer.parseInt(str[0]);
         int M = Integer.parseInt(str[1]);
-        sequence("", M, 1, N - M + 1);
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+        Arrays.sort(arr);
+
+        sequence("", M, new boolean[N]);
         System.out.println(stringBuilder);
         bufferedReader.close();
     }
 
-    static void sequence(String prev, int M, int start, int end) {
+    static void sequence(String prev, int M, boolean[] check) {
         String temp;
-        for (int i = start; i <= end; i++) {
-            temp = prev + i + " ";
+        for (int i = 0; i < N; i++) {
+            if(check[i]) continue;
+            temp = prev + arr[i] + " ";
             if (M == 1) {
                 stringBuilder.append(temp).append("\n");
             }
             else {
-                sequence(temp,M - 1, i + 1, end + 1);
+                boolean[] clone = check.clone();
+                clone[i] = true;
+                sequence(temp,M - 1, clone);
             }
         }
     }
