278a7e7bef8ef2f56585f5e71a3e0c01565a9106
s12171934 <ssong99999@naver.com>
2024-04-29 10:23:04 +0900

    11725 트리의 부모 찾기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,57 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        int [][] rgb = new int[3][N + 1];
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder stringBuilder = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        int [] parent = new int[N + 1];
 
-        for (int i = 1; i <= N; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            rgb[0][i] = Math.min(rgb[1][i - 1], rgb[2][i - 1]) + Integer.parseInt(stringTokenizer.nextToken());
-            rgb[1][i] = Math.min(rgb[0][i - 1], rgb[2][i - 1]) + Integer.parseInt(stringTokenizer.nextToken());
-            rgb[2][i] = Math.min(rgb[1][i - 1], rgb[0][i - 1]) + Integer.parseInt(stringTokenizer.nextToken());
+        ArrayList<ArrayList<Integer>> nodes = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            nodes.add(arr);
         }
 
-        System.out.println(Math.min(Math.min(rgb[0][N], rgb[1][N]), rgb[2][N]));
+        for (int i = 0; i < N - 1; i++) {
+            int a = read();
+            int b = read();
+            nodes.get(a - 1).add(b);
+            nodes.get(b - 1).add(a);
+        }
 
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int i : nodes.get(node - 1)) {
+                if(i == parent[node]) continue;
+                queue.add(i);
+                parent[i] = node;
+            }
+        }
+
+        for (int i = 2; i <= N; i++) {
+            stringBuilder.append(parent[i]).append("\n");
+        }
+
+        System.out.println(stringBuilder);
         bufferedReader.close();
+    }
+
+    static int read() throws Exception {
+        int res = 0;
+        while (true) {
+            int pointer = bufferedReader.read();
+            if (pointer == 32 || pointer == 10) {
+                return res;
+            }
+            res = 10 * res + pointer - 48;
+        }
     }
 }
